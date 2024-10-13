2162cd692593b7cb57f33d2a966ebfcba2782940
Poraparint <poraparint@gmail.com>
2024-10-13 13:28:41 +0700

    13:28 Prim

--- app/(Auth)/sign-in/page.tsx
+++ app/(Auth)/sign-in/page.tsx
@@ -14,7 +14,11 @@ function SignInPage({ searchParams }: { searchParams: { message: string } }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `http://localhost:3000/auth/callback`,
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent"
+        }
       },
     });
 
