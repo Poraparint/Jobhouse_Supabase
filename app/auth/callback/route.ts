52f6df9210def9cca4e5a0a0eca0e838d037bd30
Poraparint <poraparint@gmail.com>
2024-11-06 22:06:58 +0700

    m

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,27 +2,22 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/protected";
+  const next = "/protected"; // Redirect to protected path after login
 
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      const redirectUrl = isLocalEnv
-        ? `http://localhost:3000${next}`
-        : `https://jobhouse-supabase.vercel.app${next}`;
-
-      return NextResponse.redirect(redirectUrl);
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
-  // ถ้าไม่สามารถเปลี่ยนเส้นทางได้ให้แสดงข้อผิดพลาด
   return NextResponse.json(
     { message: "Something went wrong" },
     { status: 500 }
   );
 }
+
